08c682f6ea5de7a21be6dfaa4c39ea004fd187f1
Leonhardiv94 <149549716+Leonhardiv94@users.noreply.github.com>
2025-04-07 22:42:09 -0500

    Update funciones.js

--- funciones.js
+++ funciones.js
@@ -20,26 +20,33 @@ function queDia1(nombre){
     return `Hola ${nombre}! Feliz ${nombreDia[dia]}`;
 }
 
-function cuantosDias(año, mes, dia, NombreEvento){
-    const hoy = new Date();
-    const miliSegEnUnDia = 1000*60*60*24;
-    hoy.setHours(0,0,0,0);
-    const fechaEvento = new Date(año, mes -1, dia)
-    //const diferenciaHoras = Math.abs(fechaEvento.getTimezoneOffset()/60)-Math.abs(hoy.getTimezoneOffset()/60);
-    //console.log(diferenciaHoras);
-
-    if (fechaEvento > hoy){
-        return `Faltan ${(fechaEvento.getTime()- hoy.getTime())/miliSegEnUnDia} dias para ${NombreEvento}`;
-    } else {
-        return 'La fecha ya paso'
+function mediaConVar(numeros){
+    var suma=0;
+    for (var indice = 0; indice < numeros.length; indice +=1){
+        suma += numeros[indice];
     }
+    var media = suma/numeros.length;
+    return `Los ${numeros.length} numeros tienen suma= ${suma} y media= ${media}. El indice es ${indice}`;
 }
 
 function leeFormulario(){
     document.getElementById('output5').innerHTML = `La fecha seleccionada es: ${document.getElementById('fecha').value}`
+    const hoy = new Date();
+    const miliSegEnUnDia = 1000*60*60*24;
+    hoy.setHours(0,0,0,0);
+    const fechaEvento = (new Date(document.getElementById('fecha').value));
+    fechaEvento.setHours(24,0,0,0);
+    //const diferenciaHoras = Math.abs(fechaEvento.getTimezoneOffset()/60)-Math.abs(hoy.getTimezoneOffset()/60);
+    //hoy.setHours(hoy.getHours()- diferenciaHoras)
+    if (fechaEvento > hoy){
+        let diasTotales = (fechaEvento.getTime()- hoy.getTime())/miliSegEnUnDia;
+        diasAbsolutos= Math.abs(diasTotales);
+        document.getElementById('output4').innerHTML = `Faltan ${diasAbsolutos} dias para la fecha`;
+    } else {
+        document.getElementById('output4').innerHTML = 'La fecha ya paso'
+    }
 }
 
-document.getElementById('output4').innerHTML = cuantosDias(2025,12,25,'Navidad');
 
 document.getElementById('carita').src = valorSrc;
 document.getElementById('output1').innerHTML = mensaje;
@@ -48,3 +55,4 @@ document.getElementById('output3').innerHTML = queDia1(nombre);
 console.log(new Date());
 console.log(new Date().setHours(0,0,0,0));
 console.log(new Date(2025,12-1,25).setHours(0,0,0,0));
+console.log(mediaConVar([84,36,61,67,22,20,22,16,76,54]));
